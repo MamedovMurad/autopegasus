e038e0b9581694e126d204253831011dd340136f
MamedovMurad <murad.mammadov@hq.ferrumcapital.az>
2020-12-02 15:27:29 +0400

    add logo  and add phone numbers and add address

--- resources/views/front/Layout/header.blade.php
+++ resources/views/front/Layout/header.blade.php
@@ -42,4 +42,256 @@
 
 </head>
 
+
+
+
+    <!-- Main Wrapper Start -->
+    <!--header area start-->
+    <header class="header_area"> {{-- 
+        <!--header top start-->
+        <div class="header_top">
+            <div class="container">
+                <div class="top_inner">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-md-6">
+                            <div class="follow_us">
+                                <label>Follow Us:</label>
+                                <ul class="follow_link">
+                                    <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                                    <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                                    <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                                    <li><a href="#"><i class="ion-social-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6">
+                            <div class="top_right text-right">
+                                <ul>
+                                    <li class="top_links"><a href="#"><i class="ion-android-person"></i> My Account<i class="ion-ios-arrow-down"></i></a>
+                                        <ul class="dropdown_links">
+                                            <li><a href="checkout.html">Checkout </a></li>
+                                            <li><a href="my-account.html">My Account </a></li>
+                                            <li><a href="cart.html">Shopping Cart</a></li>
+                                            <li><a href="wishlist.html">Wishlist</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="language"><a href="#"><img src="{{asset('/front/autima/')}}/assets/img/logo/language.png" alt="">en-gb<i class="ion-ios-arrow-down"></i></a>
+                                        <ul class="dropdown_language">
+                                            <li><a href="#"><img src="{{asset('/front/autima/')}}/assets/img/logo/language.png" alt=""> English</a></li>
+                                            <li><a href="#"><img src="{{asset('/front/autima/')}}/assets/img/logo/language2.png" alt=""> Germany</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="currency"><a href="#">$ USD<i class="ion-ios-arrow-down"></i></a>
+                                        <ul class="dropdown_currency">
+                                            <li><a href="#">EUR – Euro</a></li>
+                                            <li><a href="#">GBP – British Pound</a></li>
+                                            <li><a href="#">INR – India Rupee</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+        <!--header top start-->
+            
+        <!--header bottom satrt-->
+        <div class="header_bottom sticky-header">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12">
+                        <div class="main_menu header_position">
+                            <nav>
+                                <ul>
+                                    <li><a href="javascript:void(0)">Trink sat!<i class="fa fa-angle-down"></i></a>
+                                        <ul class="sub_menu">
+                                            <li><a href="index.html">Teklif al</a></li>
+                                      {{--       <li><a href="index-2.html">Home 2</a></li>
+                                            <li><a href="index-3.html">Home 3</a></li>
+                                            <li><a href="index-4.html">Home 4</a></li>
+                                            <li><a href="index-5.html">Home 5</a></li>
+                                            <li><a href="index-6.html">Home 6</a></li>
+                                            <li class="home7new"><a href="index-7.html">Home 7</a><span>new</span></li> --}}
+                                        </ul>
+                                    </li>
+                                    <li class="mega_items"><a href="javascript:void(0)">Arac al<i class="fa fa-angle-down"></i></a>
+                                        <div class="mega_menu">
+                                            <ul class="mega_menu_inner">
+                                                <li><a href="javascript:void(0)">Kategoriler</a>
+                                                    <ul>
+                                                        <li><a href="shop-fullwidth.html">Otomobil</a></li>
+                                                        <li><a href="shop-fullwidth-list.html">Arazi, suv, pick-up</a></li>
+                                                        <li><a href="shop-right-sidebar.html">Minivan & Panelvan</a></li>
+                                                        <li><a href="shop-right-sidebar-list.html">Klasik Aracklar</a></li>
+                                                        <li><a href="shop-list.html">Elektrikli Aracklar</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="javascript:void(0)">Kasa Tipleri</a>
+                                                    <ul>
+                                                        <li><a href="cart.html">Sedan</a></li>
+                                                        <li><a href="wishlist.html">Hatcback</a></li>
+                                                        <li><a href="checkout.html">Station wagon</a></li>
+                                                        <li><a href="my-account.html">Suv/Pick-up</a></li>
+                                                        <li><a href="404.html">Coupe</a></li>
+                                                        <li><a href="404.html">Cabrio</a></li>
+                                                        <li><a href="404.html">Van/ Panelvan</a></li>
+                                                        <li><a href="404.html">Minibus</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="javascript:void(0)">Tum 2 .el Ilanlar</a>
+                                                    <ul>
+                                                        <li><a href="product-details.html">Bana arac oner</a></li>
+                                                        <li><a href="product-sidebar.html">pArabam kac para</a></li>
+                                                        <li><a href="product-grouped.html">markalar</a></li>
+                   
+
+                                                    </ul>
+                                                </li>
+                                           
+                                            </ul>
+                                            <div class="banner_static_menu">
+                                                <a href="shop.html"><img src="{{asset('/front/autima/')}}/assets/img/bg/banner1.jpg" alt=""></a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li><a href="blog.html">arac sat!<i class="fa fa-angle-down"></i></a>
+                                        <ul class="sub_menu pages">
+                                            <li><a href="blog-details.html">trink sat</a></li>
+                                            <li><a href="blog-fullwidth.html">turbolar</a></li>
+                                            <li><a href="blog-sidebar.html">arabam kac para</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Hizmetlerimiz <i class="fa fa-angle-down"></i></a>
+                                        <ul class="sub_menu pages">
+                                            <li><a href="about.html">Arabam kac para</a></li>
+                         
+                                        </ul>
+                                    </li>
+                                    <li><a href="about.html">Kompaniyalarimiz</a>
+                                        <ul class="sub_menu pages">
+                                            <li><a href="contact.html"> Autoclub arac temizliyi</a></li>
+                         
+                                        </ul>
+                                    </li>
+                                   
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!--header bottom end-->
+
+
+
+        <!--header middel start-->
+        <div class="header_middle">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="logo">
+                            <a href="index.html"><img src="{{asset('/front/autima/')}}/assets/img/auto-logo/image.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 col-md-6">
+                        <div class="middel_right">
+                            <div class="search-container">
+                                <form action="#">
+                                    <div class="search_box">
+                                        <input placeholder="Elan axtar ..." type="text">
+                                        <button type="submit"><i class="ion-ios-search-strong"></i></button>
+                                    </div>
+                                </form>
+                            </div>
+                      {{--       <div class="middel_right_info">
+
+                                <div class="header_wishlist">
+                                    <a href="wishlist.html"><span class="lnr lnr-heart"></span> Wish list </a>
+                                    <span class="wishlist_quantity">3</span>
+                                </div>
+                                <div class="mini_cart_wrapper">
+                                    <a href="javascript:void(0)"><span class="lnr lnr-cart"></span>My Cart </a>
+                                    <span class="cart_quantity">2</span>
+                                    
+                                </div>
+                            </div> --}}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--header middel end-->
+        
+{{--         <!--mini cart-->
+        <div class="mini_cart">
+            <div class="cart_close">
+                <div class="cart_text">
+                    <h3>cart</h3>
+                </div>
+                <div class="mini_cart_close">
+                    <a href="javascript:void(0)"><i class="ion-android-close"></i></a>
+                </div>
+            </div>
+            <div class="cart_item">
+                <div class="cart_img">
+                    <a href="#"><img src="{{asset('/front/autima/')}}/assets/img/s-product/product.jpg" alt=""></a>
+                </div>
+                <div class="cart_info">
+                    <a href="#">JBL Flip 3 Splasroof Portable Bluetooth 2</a>
+
+                    <span class="quantity">Qty: 1</span>
+                    <span class="price_cart">$60.00</span>
+
+                </div>
+                <div class="cart_remove">
+                    <a href="#"><i class="ion-android-close"></i></a>
+                </div>
+            </div>
+            <div class="cart_item">
+                <div class="cart_img">
+                    <a href="#"><img src="{{asset('/front/autima/')}}/assets/img/s-product/product2.jpg" alt=""></a>
+                </div>
+                <div class="cart_info">
+                    <a href="#">Koss Porta Pro On Ear Headphones </a>
+                    <span class="quantity">Qty: 1</span>
+                    <span class="price_cart">$69.00</span>
+                </div>
+                <div class="cart_remove">
+                    <a href="#"><i class="ion-android-close"></i></a>
+                </div>
+            </div>
+            <div class="mini_cart_table">
+                <div class="cart_total">
+                    <span>Sub total:</span>
+                    <span class="price">$138.00</span>
+                </div>
+                <div class="cart_total mt-10">
+                    <span>total:</span>
+                    <span class="price">$138.00</span>
+                </div>
+            </div>
+
+            <div class="mini_cart_footer">
+                <div class="cart_button">
+                    <a href="cart.html">View cart</a>
+                </div>
+                <div class="cart_button">
+                    <a class="active" href="checkout.html">Checkout</a>
+                </div>
+
+            </div>
+
+        </div>
+        <!--mini cart end--> --}}
+    
+    </header>
+    <!--header area end--
+
 <body>
